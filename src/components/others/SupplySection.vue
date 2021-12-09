0385bfde86c3f734696021894e8296ef8099e9e8
worlddeleteRin <worlddelete0@gmail.com>
2021-12-09 23:34:03 +0300

    :lipstick: make supply section better

--- src/components/others/SupplySection.vue
+++ src/components/others/SupplySection.vue
@@ -4,6 +4,17 @@
     <div class="supplyListWrapper">
       <!-- supply list -->
       <div class="supplyList">
+        <div class="supplyList__header">
+          <div class="supplyList__header__item supplyList__header__item__first">
+            Token
+          </div>
+          <div class="supplyList__header__item supplyList__header__item__second">
+            Available Amount
+          </div>
+          <div class="supplyList__header__item supplyList__header__item__third">
+            Percentage
+          </div>
+        </div>
         <!-- supply item -->
         <div v-for="token in supplyTokens" :key="token.id" class="supplyItem">
           <div class="supplyItem__token">
@@ -42,60 +53,32 @@ export default {
           percentage: '123123 %',
         },
         {
-          id: 1,
-          imgsrc: 'binance.svg',
+          id: 2,
+          imgsrc: 'avalanche.svg',
           title: 'ERC-20',
           availableAmount: '123123',
-          percentage: '123123 %',
+          percentage: '234234 %',
         },
         {
-          id: 1,
-          imgsrc: 'binance.svg',
+          id: 3,
+          imgsrc: 'polygon.svg',
           title: 'ERC-20',
           availableAmount: '123123',
-          percentage: '123123 %',
+          percentage: '234234 %',
         },
         {
-          id: 1,
-          imgsrc: 'binance.svg',
-          title: 'ERC-20',
+          id: 4,
+          imgsrc: 'neo.svg',
+          title: 'NEP-17',
           availableAmount: '123123',
-          percentage: '123123 %',
+          percentage: '234234 %',
         },
         {
-          id: 1,
-          imgsrc: 'binance.svg',
-          title: 'ERC-20',
+          id: 4,
+          imgsrc: 'phantasma.svg',
+          title: 'PEPE-12',
           availableAmount: '123123',
-          percentage: '123123 %',
-        },
-        {
-          id: 1,
-          imgsrc: 'binance.svg',
-          title: 'ERC-20',
-          availableAmount: '123123',
-          percentage: '123123 %',
-        },
-        {
-          id: 1,
-          imgsrc: 'binance.svg',
-          title: 'ERC-20',
-          availableAmount: '123123',
-          percentage: '123123 %',
-        },
-        {
-          id: 1,
-          imgsrc: 'binance.svg',
-          title: 'ERC-20',
-          availableAmount: '123123',
-          percentage: '123123 %',
-        },
-        {
-          id: 1,
-          imgsrc: 'binance.svg',
-          title: 'ERC-20',
-          availableAmount: '123123',
-          percentage: '123123 %',
+          percentage: '234234 %',
         },
       ],
     };
@@ -124,30 +107,85 @@ export default {
   margin: auto;
   border-radius: 12px;
   box-shadow: 0px 0px 17px -7px rgba(34, 60, 80, 0.2);
+  overflow: hidden;
+  &__header {
+    background: #7466ff;
+    display: flex;
+    height: 40px;
+    align-items: center;
+    justify-content: center;
+    &__item {
+      color: white;
+      font-size: 13px;
+      display: flex;
+      justify-content: center;
+      &__first {
+        width: 45%;
+      }
+      &__second {
+        width: 25%;
+      }
+      &__third {
+        width: 25%;
+      }
+    }
+  }
 }
 .supplyItem {
   height: 55px;
   border-bottom: 2px solid #f8f8f8;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
   &__token {
     display: flex;
     align-items: center;
-    width: 40%;
+    justify-content: center;
+    width: 45%;
     &__image {
-      width: 30px;
+      width: 20px;
+      height: 20px;
     }
     &__title {
       font-size: 12px;
+      margin-left: 7px;
     }
   }
   &__amount {
+    width: 25%;
     font-size: 14px;
+    display: flex;
+    justify-content: center;
   }
   &__percentage {
+    width: 25%;
     font-size: 14px;
+    display: flex;
+    justify-content: center;
+  }
+}
+
+@media (max-width: 640px) {
+  .supplyList {
+    &__header {
+      &__item {
+        font-size: 10px;
+      }
+    }
+  }
+  .supplyItem {
+    &__token {
+      &__title {
+        font-size: 10px;
+      }
+    }
+    &__amount {
+      font-size: 12px;
+    }
+    &__percentage {
+      font-size: 11px;
+    }
   }
 }
 </style>
