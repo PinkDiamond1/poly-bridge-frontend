ba1fdc7cd1cfe6f2dd4b94202d816003e97b6d13
vincentgeneste <vincentgeneste@gmail.com>
2021-12-21 10:57:59 -0800

    hide supply before token sale

--- src/components/others/SupplySection.vue
+++ src/components/others/SupplySection.vue
@@ -142,12 +142,21 @@ export default {
       .then(res => {
         const circulating = res.all_circulating_supply === 0 ? 1 : res.all_circulating_supply;
         this.totalSupplyCount = number(res.all_total_supply, 0);
-        this.availableSupplyCount = number(res.all_circulating_supply, 0);
+        /* this.availableSupplyCount = number(res.all_circulating_supply, 0);
         this.supplyTokens.forEach(token => {
           try {
             const tc = res[`${token.slug}_circulating_supply`];
             token.availableAmount = number(tc, 0);
             token.percentage = `${((100 * tc) / circulating).toFixed(2)}%`;
+          } catch (err) {
+            console.error(err);
+          }
+        }); */
+        this.availableSupplyCount = 0;
+        this.supplyTokens.forEach(token => {
+          try {
+            token.availableAmount = number(0, 0);
+            token.percentage = `${((100 * 0) / circulating).toFixed(2)}%`;
           } catch (err) {
             console.error(err);
           }
