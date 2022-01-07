635943627f82047a81cb70260414c4a0ef197be5
vincentgeneste <vincentgeneste@gmail.com>
2022-01-06 18:04:42 -0800

    added addToMetamask feature except polygon

--- src/views/home/Form.vue
+++ src/views/home/Form.vue
@@ -530,18 +530,23 @@ export default {
   },
   methods: {
     async addToMetamask(chain) {
-      Message({ message: 'Not available yet', type: 'error', duration: 5000 });
-      return false;
-      /*
+      if (chain === 'polygon') {
+        Message({ message: 'Not available yet', type: 'error', duration: 5000 });
+        return false;
+      }
       let tokenAddress = '';
       const isMainnet = false;
-      if (chain === 'bsc' && isMainnet === true) tokenAddress = '0x0B53b5dA7d0F275C31a6A182622bDF02474aF253';
+      if (chain === 'bsc' && isMainnet === true)
+        tokenAddress = '0x0B53b5dA7d0F275C31a6A182622bDF02474aF253';
       if (chain === 'bsc') tokenAddress = '0xf3fd0f360ace3b0e83843221a763fec857291060';
-      if (chain === 'eth' && isMainnet === true) tokenAddress = '0x35609dC59E15d03c5c865507e1348FA5abB319A8';
+      if (chain === 'eth' && isMainnet === true)
+        tokenAddress = '0x35609dC59E15d03c5c865507e1348FA5abB319A8';
       if (chain === 'eth') tokenAddress = '0x26D583e2CDa958b13CC319FAd124aa729f8A196e';
-      if (chain === 'polygon' && isMainnet === true) tokenAddress = '0x0B53b5dA7d0F275C31a6A182622bDF02474aF253';
+      if (chain === 'polygon' && isMainnet === true)
+        tokenAddress = '0x0B53b5dA7d0F275C31a6A182622bDF02474aF253';
       if (chain === 'polygon') tokenAddress = '0x957404188EA8804eFF6dc052e6B35c58aE351357';
-      if (chain === 'avalanche' && isMainnet === true) tokenAddress = '0x0B53b5dA7d0F275C31a6A182622bDF02474aF253';
+      if (chain === 'avalanche' && isMainnet === true)
+        tokenAddress = '0x0B53b5dA7d0F275C31a6A182622bDF02474aF253';
       if (chain === 'avalanche') tokenAddress = '0x7D35e9D90bD91BA82dAe43d7e03cF1e04c14aea8';
       const win = window;
       const tokenSymbol = 'GM';
@@ -560,7 +565,6 @@ export default {
         },
       });
       return call;
-      */
     },
     changeTokenBasicName(tokenBasicName) {
       this.tokenBasicName = tokenBasicName;
